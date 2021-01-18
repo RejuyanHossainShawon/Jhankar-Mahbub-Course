364430b69d397976a3586bb73f66f38e86e5bd5c
ukwl <ukwldeveloper02@gmail.com>
2021-01-18 13:18:58 +0600

    course

---
+++ milestone 3 js/module 12/conditional 12-9.js
@@ -0,0 +1,19 @@
+var price = 19;
+if (price <= 18){
+    console.log("This is under my range");
+}
+else{
+    console.log("This is out of my range");
+}
+
+var job =false;
+var saving = 10000;
+if(job == true && saving>1000){
+    console.log("biya korte paro");
+}
+else if(job == true || saving>1000){
+    console.log("BIya korar kotha bhava jay");
+}
+else{
+    console.log("Tur kopale BIya namok sobdo o nai");
+}
---
+++ milestone 3 js/module 12/hello.js
@@ -0,0 +1,29 @@
+// console.log(7);
+// var time = 10;
+// var semester = 8;
+// var person = "Shawon , Alamin , Rifat ,Rakin";
+// var speed = 20;
+// var youAreLier= true;
+// var youAre = false;
+// console.log( typeof time,semester, typeof person,speed,  youAre);
+// var sentence = "I want to be a good web developer";
+// sentence = sentence.toLowerCase();
+// console.log(sentence.indexOf('good'));
+// console.log(sentence.split(' '));
+
+// var para ="I am a javascript developer";
+// console.log(para.indexOf('j'));
+var num1 = 12.5;
+var num2 = 15.2;
+// num2 = +num2;
+// num2= parseFloat(num2);
+// var total = num1+ num2;
+// total = total.toFixed(2);
+// console.log(total);
+num2=Math.floor(num2);
+console.log(num2);
+
+
+
+
+
